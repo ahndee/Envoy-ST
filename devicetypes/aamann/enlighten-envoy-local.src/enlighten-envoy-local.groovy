cfada5ee12797de278516d73ba44e764f34cd3f9
Andreas Amann <aamann@mac.com>
2016-05-24 20:21:44 -0700

    - whitespace normalization

--- devicetypes/aamann/enlighten-envoy-local.src/enlighten-envoy-local.groovy
+++ devicetypes/aamann/enlighten-envoy-local.src/enlighten-envoy-local.groovy
@@ -25,13 +25,13 @@ preferences {
 		defaultValue:"80", required: true, displayDuringSetup: true)
 	input("confNumInverters", "number", title:"Number of Inverters/Panels",
 		required: true, displayDuringSetup: true)
-    input(title: "", description: "Inverter Size (W)\n\nRated maximum power in Watts for each inverter\n\nUse '225' for M215 and '250' for M250", type: "paragraph", element: "paragraph", displayDuringSetup: true)
+	input(title: "", description: "Inverter Size (W)\n\nRated maximum power in Watts for each inverter\n\nUse '225' for M215 and '250' for M250", type: "paragraph", element: "paragraph", displayDuringSetup: true)
 	input("confInverterSize", "number", title:"",
 		required: true, displayDuringSetup: true)
-    input(title: "", description: "Panel Size (W)\n\nRated maximum power in Watts for each panel\n\nThis can be different than the maximum inverter power above", type: "paragraph", element: "paragraph", displayDuringSetup: true)
+	input(title: "", description: "Panel Size (W)\n\nRated maximum power in Watts for each panel\n\nThis can be different than the maximum inverter power above", type: "paragraph", element: "paragraph", displayDuringSetup: true)
 	input("confPanelSize", "number", title:"",
 		required: true, displayDuringSetup: true)
-    input(title:"", description: "Version: ${version()}", type: "paragraph", element: "paragraph")
+	input(title:"", description: "Version: ${version()}", type: "paragraph", element: "paragraph")
 }
 
 metadata {
